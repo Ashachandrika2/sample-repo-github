231f5a8c22dd232431135ff3835878108dff1992
Ashachandrika2 <135569793+Ashachandrika2@users.noreply.github.com>
2025-03-29 12:36:04 +0530

    Add files via upload

---
+++ my_script.py
@@ -0,0 +1,24 @@
+# python script demonstrating proper syntax, comments , data types, numbers, and casting
+
+# Declaring variablrs of different data types
+name = "Asha"         # String
+age  = "29"           # Integer
+height= 5.2           # Float
+is_student = false    # Boolean 
+
+# printing the variables
+print ( "Name:", name)
+print("Age:", age)
+print("Height:",  height)
+print("Is_student:", is_student)
+
+# performing a basic arithmetic operation
+a = 10
+b = 5
+c = a+b
+print("sum of " a, "and"  b, "is", c)
+
+
+# Demonstrating type casting (converting integer to string)
+ age_str = str(age)     # converting integer to string
+ print("Age as a string:" age_str)
